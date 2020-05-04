fd2eccd4489f38bb2a1fc7b1bb202935d4116754
Felipe Santaniello <felipe.santaniello-ext@viavarejo.com.br>
2020-05-04 16:15:23 -0300

    Initial Commit

---
+++ freightgateway/src/main/java/com/dddms/freightgateway/FreightgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dddms.freightgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightgatewayApplication.class, args);
+	}
+
+}
---
+++ freightgateway/src/test/java/com/dddms/freightgateway/FreightgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dddms.freightgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
